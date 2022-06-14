d85b991c3e147d0869d282b8e8fb7a9f100f5390
KpUser12 <106372852+KpUser12@users.noreply.github.com>
2022-06-14 14:11:59 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,10 @@
-print ("Hello, World!")
-print ("Hello, kapstone!")
+public class Test {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Hello world in java");
+		System.out.println("Hello kaps team");
+	}
+
+}
